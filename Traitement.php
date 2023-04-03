8028a22e786c338f85dd5dd577e0bddeb3a9051a
nibane <azari46165@gmail.com>
2023-04-03 16:58:34 +0200

    interraction formulaire

--- Traitement.php
+++ Traitement.php
@@ -1,56 +1,63 @@
 <?php
 session_start();
+$_SESSION['message'] = "";
 
 
 if (isset($_GET['action'])) {
     switch($_GET['action']){
         case "addProduct" : 
-            
             if (isset($_POST['submit'])) {
-
+                $_SESSION['message'] = "element ajouter";
                 $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
                 $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
                 $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+                
+                    if ($name && $price && $qtt) {
 
-                if ($name && $price && $qtt) {
-
-                    $product = [
-                        "name" => $name,
-                        "price" => $price,
-                        "qtt" => $qtt,
-                        "total" => $price * $qtt,
-                    ];
-
-                    $_SESSION["products"][] = $product;
-                }
-
+                        $product = [
+                            "name" => $name,
+                            "price" => $price,
+                            "qtt" => $qtt,
+                            "total" => $price * $qtt,
+                        ];
+                        
+                        $_SESSION["products"][] = $product;
+                    }
+                    else{
+                        $_SESSION['message'] = "ERREUR";
+                    }
+                
             }
-        break;
+            break;
         case "augmenterQtt":
-            if (isset($_GET['id'])&& isset($_SESSION["products"][$_GET['id']])){
-
-                $_SESSION["products"][$_GET['id']]['qtt']++;
-                $_SESSION["products"][$_GET['id']]['total'] = $_SESSION["products"][$_GET['id']]['qtt'] * $_SESSION["products"][$_GET['id']]['price'];
-
-            }
-            header("Location:Recap.php");
-            die();
-
+             if (isset($_GET['id'])&& isset($_SESSION["products"][$_GET['id']])){
+                 
+                 $_SESSION["products"][$_GET['id']]['qtt']++;
+                 $_SESSION["products"][$_GET['id']]['total'] = $_SESSION["products"][$_GET['id']]['qtt'] * $_SESSION["products"][$_GET['id']]['price'];
+                 $_SESSION['message'] = "plus";
+                 
+                }
+                header("Location:Recap.php");
+                die();
+                
         case "baisserQtt":
             if (isset($_GET['id'])&& isset($_SESSION["products"][$_GET['id']])){
-            
+                        
                 $_SESSION["products"][$_GET['id']]['qtt']--;
                 $_SESSION["products"][$_GET['id']]['total'] = $_SESSION["products"][$_GET['id']]['qtt'] * $_SESSION["products"][$_GET['id']]['price'];
+                
             }
-            header("Location:Recap.php");
-            die();
-            
-        
-            
-        }}
+                header("Location:Recap.php");
+                die();
+                    
+                    
+                    
+    }
+}
+                
+                
 
-
-
-$_SESSION['Message'] = "sdfqdsf"
-
-header("Location:index.php");
+                
+                header("Location:index.php");
+                
+                
