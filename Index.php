f45e5f758c1ff276b813b3458c46da3ce7c025c6
nibane <azari46165@gmail.com>
2023-04-03 16:57:00 +0200

    ajout liens bootstrap, ajout message avec code php

--- Index.php
+++ Index.php
@@ -6,12 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title>Ajout produit</title>
 </head>
 
 <body>
-    
+    <?php
+        
+        session_start();
+        $message = (isset($_SESSION['message'])) ? $_SESSION['message'] : null;
+        echo $message;
+        unset($message);
+
+        ?>
     <nav>
 
         <div><a href="Index.php">Index</a></div>
@@ -22,24 +30,25 @@
     
     <h1>Ajouter un produit</h1>
     <form action="traitement.php?action=addProduct" method="post">
+        
         <p>
             <label>
                 Nom du produit :
-                <input type="text" name="name">
+                <input type="text" name="name"  class="form-control">
             </label>
         </p>
 
         <p>
             <label>
                 Prix du produit :
-                <input type="number" step="any" name="price">
+                <input type="number" step="any" name="price"  class="form-control">
             </label>
         </p>
 
         <p>
             <label>
                 Quantité désirée :
-                <input type="number" name="qtt" value="1">
+                <input type="number" name="qtt" value="1"  class="form-control">
             </label>
         </p>
 
@@ -48,6 +57,9 @@
         </p>
 
     </form>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
 
