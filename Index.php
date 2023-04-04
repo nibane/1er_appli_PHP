49886ede41c568d99b1717dba038814f19ae281d
nibane <azari46165@gmail.com>
2023-04-04 11:58:54 +0200

    ajout require_once et fonction total_qtt()

--- Index.php
+++ Index.php
@@ -13,8 +13,10 @@
 
 <body>
     <?php
-        
         session_start();
+
+        require_once "Fonction.php";
+
         $message = (isset($_SESSION['message'])) ? $_SESSION['message'] : null;
         echo $message;
         unset($message);
@@ -57,6 +59,8 @@
         </p>
 
     </form>
+
+    <?=total_qtt() ?>
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
