7dcbd827a93ce10ee52516ffffaa1e0e0b349cf0
nibane <azari46165@gmail.com>
2023-04-04 11:58:11 +0200

    corection faute de frappe

--- Fonction.php
+++ Fonction.php
@@ -1,7 +1,7 @@
-<?=
+<?php
 
-    function total_qtt(){
-        $total_qtt =;
+    function total_qtt() {
+        $total_qtt = 0;
         if(isset($_SESSION{"products"})){
             foreach($_SESSION["products"] as $product) {
                 $total_qtt += $product["qtt"];
